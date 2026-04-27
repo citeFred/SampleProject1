a724fa1fd3ebcbcd0ba4286c6da0a45576448edc
citefred <citefred@gmail.com>
2026-04-27 15:16:49 +0900

    feat: 동적할당(new)과 delete 키워드 소멸 확인

--- SampleProject1/Monster.h
+++ SampleProject1/Monster.h
@@ -11,6 +11,7 @@ private:
 		
 public:
     Monster(const string& name, int str, int dex, int vit, int eng, int expReward, int lv = 1);
+    virtual ~Monster(); // 소멸자 타이밍 확인용
     
     string GetName() const { return name; }
     int GetExpReward() const { return expReward; }
